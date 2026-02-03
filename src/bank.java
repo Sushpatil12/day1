a73bfc638d0cf208e9db50dd1da3660dbb6c868d
Sushma Sushma <sushpatil3581@gmail.com>
2026-02-03 21:47:39 +0530

    Update bank.java

--- src/bank.java
+++ src/bank.java
@@ -6,16 +6,17 @@ public class bank {
 
     public boolean transfer(Account acc1, Account acc2, float amount) {
         try {
-            float res = acc1.withdraw(amount);
+            acc1.withdraw(amount);
         } catch (InsufficientBalanceException insufficientBalanceException) {
             logger.severe(insufficientBalanceException.getMessage());
-            return false;
         } catch (IllegalArgumentException illegalArgumentException) {
             logger.severe(illegalArgumentException.getMessage());
         }
-        acc2.deposit(amount);
-        return true;
-    }
+        try{    
+        acc2.deposit(amount);   
+    } catch (IllegalArgumentException illegalArgumentException) {
+            logger.severe(illegalArgumentException.getMessage());
+     }
 }
 
 
